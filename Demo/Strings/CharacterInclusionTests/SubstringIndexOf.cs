c64b9fd52e733540c8673b3a262c807e430143ac
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-03-23 11:03:42 +0100

    Add example with Contains/IndexOf/Substring

---
+++ Demo/Strings/CharacterInclusionTests/SubstringIndexOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterInclusionTests
+{
+    class SubstringIndexOf
+    {
+        public string AfterSlash(string argument)
+        {
+            Contract.Requires(argument.Contains("/"));
+
+            return argument.Substring(argument.IndexOf('/'));
+        }
+    }
+}
