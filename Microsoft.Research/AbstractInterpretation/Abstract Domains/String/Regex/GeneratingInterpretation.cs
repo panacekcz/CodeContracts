c8670b617b31e1150b57e3c5b52e8b4ce451a24d
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2017-04-14 21:39:01 +0200

    Add IsEnd flag to generating interpretation

--- Microsoft.Research/AbstractInterpretation/Abstract Domains/String/Regex/GeneratingInterpretation.cs
+++ Microsoft.Research/AbstractInterpretation/Abstract Domains/String/Regex/GeneratingInterpretation.cs
@@ -1,4 +1,20 @@
-﻿using Microsoft.Research.Regex;
+﻿// CodeContracts
+// 
+// Copyright (c) Charles University (2016-2017)
+// 
+// All rights reserved. 
+// 
+// MIT License
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Created by Vlastimil Dort (2016-2017)
+
+using Microsoft.Research.Regex;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,22 +33,26 @@ namespace Microsoft.Research.AbstractDomains.Strings.Regex
         /// <summary>
         /// The abstract state representing a language of strings where a match exists.
         /// </summary>
-        public TState Open { get; set; }
+        public TState Open { get; private set; }
         /// <summary>
         /// The abstract state representing a language of strings where the match ends at
         /// the current position.
         /// </summary>
-        public TState Closed { get; set; }
+        public TState Closed { get; private set; }
 
-        public GeneratingState(TState open, TState closed)
+        public bool IsEnd { get; private set; }
+
+        public GeneratingState(TState open, TState closed, bool isEnd)
         {
             Open = open;
             Closed = closed;
+            IsEnd = isEnd;
         }
-        public GeneratingState(TState both)
+        public GeneratingState(TState both, bool isEnd)
         {
             Open = both;
             Closed = both;
+            IsEnd = isEnd;
         }
 
         public override string ToString()
@@ -60,7 +80,7 @@ namespace Microsoft.Research.AbstractDomains.Strings.Regex
         {
             get
             {
-                return new GeneratingState<TState>(operations.Bottom);
+                return new GeneratingState<TState>(operations.Bottom, true);
             }
         }
 
@@ -68,20 +88,22 @@ namespace Microsoft.Research.AbstractDomains.Strings.Regex
         {
             get
             {
-                return new GeneratingState<TState>(operations.Top);
+                return new GeneratingState<TState>(operations.Top, false);
             }
         }
 
         public GeneratingState<TState> AddChar(GeneratingState<TState> prev, CharRanges must, CharRanges can)
         {
+            if (prev.IsEnd)
+                return Bottom;
+
             CharRanges ranges = operations.IsUnderapproximating ? must : can;
-            return new GeneratingState<TState>(operations.AddChar(prev.Closed, ranges, false), operations.AddChar(prev.Closed, ranges, true));
+            return new GeneratingState<TState>(operations.AddChar(prev.Closed, ranges, false), operations.AddChar(prev.Closed, ranges, true), false);
         }
 
         public GeneratingState<TState> AssumeEnd(GeneratingState<TState> prev)
         {
-            //TODO: VD: if underapprox, should check that nothing else follows
-            return new GeneratingState<TState>(prev.Closed);
+            return new GeneratingState<TState>(prev.Closed, true);
         }
 
         public GeneratingState<TState> AssumeStart(GeneratingState<TState> prev)
@@ -90,7 +112,7 @@ namespace Microsoft.Research.AbstractDomains.Strings.Regex
 
             if (operations.CanBeEmpty(d))
             {
-                return new GeneratingState<TState>(operations.Top, operations.Empty);
+                return new GeneratingState<TState>(operations.Top, operations.Empty, prev.IsEnd);
             }
             else
             {
@@ -105,7 +127,7 @@ namespace Microsoft.Research.AbstractDomains.Strings.Regex
 
         public GeneratingState<TState> BeginLoop(GeneratingState<TState> prev, IndexInt min, IndexInt max)
         {
-            return new GeneratingState<TState>(operations.Top, operations.Empty);
+            return new GeneratingState<TState>(operations.Top, operations.Empty, false);
         }
 
         public GeneratingState<TState> EndLookaround(GeneratingState<TState> prev, GeneratingState<TState> next, bool behind)
@@ -126,14 +148,19 @@ namespace Microsoft.Research.AbstractDomains.Strings.Regex
                 loopedOpen = operations.Join(prev.Open, loopedOpen, false);
             }
 
-            return new GeneratingState<TState>(loopedOpen, loopedClosed);
-            //TODO: VD: check code above
+            return new GeneratingState<TState>(loopedOpen, loopedClosed, false);
+            //TODO: VD: check code above (including isEnd)
             //throw new NotImplementedException();
         }
 
         public GeneratingState<TState> Join(GeneratingState<TState> left, GeneratingState<TState> right, bool widen)
         {
-            return new GeneratingState<TState>(operations.Join(left.Open, right.Open, widen), operations.Join(left.Closed, right.Closed, widen));
+            var open = operations.Join(left.Open, right.Open, widen);
+            var closed = operations.Join(left.Closed, right.Closed, widen);
+
+            var isEnd = operations.IsUnderapproximating ? left.IsEnd || right.IsEnd : left.IsEnd && right.IsEnd;
+
+            return new GeneratingState<TState>(open, closed, isEnd);
         }
 
         public GeneratingState<TState> Unknown(GeneratingState<TState> data)
